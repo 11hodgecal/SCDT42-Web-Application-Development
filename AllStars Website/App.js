cff8df3585545a4dce36c05792230a5b1d40fafb
11hodgecal <57405575+11hodgecal@users.noreply.github.com>
2020-05-27 13:51:53 +0000

    Add files via upload

--- AllStars Website/App.js
+++ AllStars Website/App.js
@@ -1,7 +1,8 @@
-var Taunton = {lat: 51.014696, lng: -3.099573};
-var Western = {lat: 51.347516, lng: -2.980051};
-var bristol = {lat: 51.455763, lng: -2.605063};
-var exeter = {lat: 50.71984, lng: -3.53019};
+var Taunton = {
+    lat: 51.014696,
+    lng: -3.099573
+};
+
 var tbtn = document.getElementById("1");
 var wbtn = document.getElementById("4");
 var bbtn = document.getElementById("3");
@@ -9,62 +10,56 @@ var ebtn = document.getElementById("2");
 var LocChouice
 
 
-tbtn.onclick = function(){
-    
-    
+tbtn.onclick = function () {
+
+
     document.getElementById("venue").style.display = "block";
     document.getElementById("home").style.display = "none";
     document.getElementById("venue-details").innerHTML = "Taunton Venue Details";
-    document.getElementById("Details").innerHTML = "OPEN MONDAY 10:00 - LATE<br> TUESDAY 10:00 - LATE<br>WEDNESDAY 10:00 - LATE<br>THURSDAY 10:00 - LATE<br>FRIDAY 10:00 - LATE<br>SATURDAY 10:00 - LATE<br>SUNDAY 10:00 - LATE<br><br>Tel:01823 334880 <br> Email: taunton@allstarssportsbars.co.uk </p>";
-    
+    document.getElementById("Details").innerHTML = "OPEN MONDAY 10:00 - LATE<br> TUESDAY 10:00 - LATE<br>WEDNESDAY 10:00 - LATE<br>THURSDAY 10:00 - LATE<br>FRIDAY 10:00 - LATE<br>SATURDAY 10:00 - LATE<br>SUNDAY 10:00 - LATE<br><br>Tel:01823 334880 <br> Email: taunton@allstarssportsbars.co.uk<br><br> Address: 22 East Street Taunton TA1 3LP</p>";
+
 
 };
 
-wbtn.onclick = function(){
-    
+wbtn.onclick = function () {
+
     document.getElementById("venue").style.display = "block";
     document.getElementById("home").style.display = "none";
     document.getElementById("venue-details").innerHTML = "Western-Super-Mare Venue Details"
-    document.getElementById("Details").innerHTML = "OPEN MONDAY 10:00 - LATE<br> TUESDAY 10:00 - LATE<br>WEDNESDAY 10:00 - LATE<br>THURSDAY 10:00 - LATE<br>FRIDAY 10:00 - LATE<br>SATURDAY 10:00 - LATE<br>SUNDAY 10:00 - LATE<br><br>Tel:01934 419111 <br> Email:  weston@allstarssportsbars.co.uk </p>";
-   ;
-    
+    document.getElementById("Details").innerHTML = "OPEN MONDAY 10:00 - LATE<br> TUESDAY 10:00 - LATE<br>WEDNESDAY 10:00 - LATE<br>THURSDAY 10:00 - LATE<br>FRIDAY 10:00 - LATE<br>SATURDAY 10:00 - LATE<br>SUNDAY 10:00 - LATE<br><br>Tel:01934 419111 <br> Email:  weston@allstarssportsbars.co.uk <br><br> Address: 12-16 Regent St Weston-super-Mare BS23 1SQ</p>";
+    initMap();
+
 
 };
 
-bbtn.onclick = function(){
-    
-    
+bbtn.onclick = function () {
+
+
     document.getElementById("venue").style.display = "block";
     document.getElementById("home").style.display = "none";
     document.getElementById("venue-details").innerHTML = "Bristol Venue Details";
-    document.getElementById("Details").innerHTML = "OPEN MONDAY 10:00 - LATE<br> TUESDAY 10:00 - LATE<br>WEDNESDAY 10:00 - LATE<br>THURSDAY 10:00 - LATE<br>FRIDAY 10:00 - LATE<br>SATURDAY 10:00 - LATE<br>SUNDAY 10:00 - LATE<br><br>Tel: 01179 294295 <br> Email:  bristol@allstarssportsbars.co.uk</p>";
+    document.getElementById("Details").innerHTML = "OPEN MONDAY 10:00 - LATE<br> TUESDAY 10:00 - LATE<br>WEDNESDAY 10:00 - LATE<br>THURSDAY 10:00 - LATE<br>FRIDAY 10:00 - LATE<br>SATURDAY 10:00 - LATE<br>SUNDAY 10:00 - LATE<br><br>Tel: 01179 294295 <br> Email:  bristol@allstarssportsbars.co.uk<br><br> Address: 15-19 Queens Road Bristol BS8 1QE</p>";
     initMap();
-    
+
 
 };
 
-ebtn.onclick = function(){
-    
-    
+ebtn.onclick = function () {
+
+
     document.getElementById("venue").style.display = "block";
     document.getElementById("home").style.display = "none";
     document.getElementById("venue-details").innerHTML = "Exeter Venue Details"
-    document.getElementById("Details").innerHTML = innerHTML = "OPEN MONDAY 10:00 - LATE<br> TUESDAY 10:00 - LATE<br>WEDNESDAY 10:00 - LATE<br>THURSDAY 10:00 - LATE<br>FRIDAY 10:00 - LATE<br>SATURDAY 10:00 - LATE<br>SUNDAY 10:00 - LATE<br><br>Tel: 01392 438853 <br> Email:exeter@allstarssportsbars.co.uk </p>";
-    
+    document.getElementById("Details").innerHTML = innerHTML = "OPEN MONDAY 10:00 - LATE<br> TUESDAY 10:00 - LATE<br>WEDNESDAY 10:00 - LATE<br>THURSDAY 10:00 - LATE<br>FRIDAY 10:00 - LATE<br>SATURDAY 10:00 - LATE<br>SUNDAY 10:00 - LATE<br><br>Tel: 01392 438853 <br> Email:exeter@allstarssportsbars.co.uk<br><br> Address: 19-23 Sidwell Street Exeter EX4 6NN</p>";
+
 
 };
 
-function initMap() 
-{
-    
+function initMap() {
+
     var map = new google.maps.Map(document.getElementById("map"), {
         center: Taunton,
         zoom: 17
-    
+
     });
 }
-
-
-
-
-
